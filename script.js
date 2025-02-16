8687aeb1819ea97567f1a69c1809804c5112b7bc
DanKRT-Star <tonyle1207@gmail.com>
2025-02-16 09:53:41 +0700

    update web

--- script.js
+++ script.js
@@ -2,3 +2,18 @@ function openMenu() {
     const nav = document.querySelector('nav');
     nav.classList.toggle('open');
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+    const elements = document.querySelectorAll(".reveal");
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("visible");
+            }
+        });
+    }, { threshold: 0.2 });
+
+    elements.forEach(el => observer.observe(el));
+});
+
+
